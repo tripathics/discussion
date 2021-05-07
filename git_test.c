40c383e9814a75c69d29914a1c8d6c606b6c00f4
tripathics <tripathics@outlook.com>
2021-05-07 19:52:51 +0530

    largest number in 5x5 matrix

--- git_test.c
+++ git_test.c
@@ -2,6 +2,12 @@
 
 int main(void)
 {
-    printf("hello, world\n");
+    int arr[] = {1,2,3,4,5,6,7,8,9,10};
+
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%i%s", arr[i], (i != 9) ? ", " : "\n");
+    }
+
     return 0;
 }
