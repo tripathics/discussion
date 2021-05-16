c2af3677109ac92ce6ff14bdef7f9c409a934fe8
Pursottam Sah <rahulsah6003@gmail.com>
2021-05-16 21:24:33 +0530

    add credit card.c

---
+++ credit_card.c
@@ -0,0 +1,59 @@
+
+/*********
+A Credit Card number is usually a 16-digit number. A valid Credit
+Card number would satisfy a rule explained below with the help of a
+dummy Credit Card number—4567 1234 5678 9129. Start with the
+rightmost - 1 digit and multiply every other digit by 2.
+4 5 6 7 1 2 3 4 5 6 7 8 9 1 2 9
+8   12  2   6   10   14  18  4
+Then subtract 9 from any number larger than 10. Thus we get:
+8 3 2 6 1 5 9 4
+Add them all up to get 38.
+Add all the other digits to get 42.
+Sum of 38 and 42 is 80. Since 80 is divisible by 10, the Credit Card
+number is valid.
+Write a program that receives a Credit Card number and checks
+using the above rule whether the Credit Card number is valid.
+************/
+
+#include<stdio.h>
+#include <string.h>
+int muliply( char card_2);
+
+int main()
+{
+
+    char card[20];
+
+    printf("Enter your  card number [ SBI credit card ]");
+
+    scanf("%s",card);
+    
+    if(strlen(card) != 16 || strlen(card) <2)
+    {
+        printf("Enter a valid card number");
+        
+        return 1;
+    }
+    
+   
+    multiply(card);
+
+}
+
+int muliply( char card_2)
+{
+    int temp =0;
+
+    for(int i=0; i<strlen(card_2);i+=2;)
+    {
+        temp +=  2*card_2[i];
+    }
+
+    if(temp>= 10)
+    {
+        temp =temp-9;
+    }
+
+    
+}
